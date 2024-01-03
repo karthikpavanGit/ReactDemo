e2e33de085f9b14d3c38bc196acbcb595fa13a69
PavanKarthik <karthikpavan370@gmail.com>
2024-01-04 00:21:21 +0530

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='Nav'>
      <h1>Pavan Karthik Mandadapu</h1>
-     <p>ZeroOne Code club</p>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>About</li>
+          <li>Service</li>
+          <li>Work</li>
+          <li>Resume</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+     </div>
+     <div className="Hero">
+      <h1>Hello!</h1>
+     </div>
     </div>
   );
 }
