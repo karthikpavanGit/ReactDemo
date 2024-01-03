9a5868f178306677b6e15b6e7372af7d6422be9f
PavanKarthik <karthikpavan370@gmail.com>
2024-01-03 20:11:13 +0530

    files added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Pavan Karthik Mandadapu</h1>
+     <p>ZeroOne Code club</p>
+    </div>
+  );
+}
+
+export default App;
